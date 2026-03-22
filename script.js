6e85585c8fc406ebacf9e28665f6fce826419311
DDBBrelax <2830345851@qq.com>
2026-03-22 18:49:25 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-// 点击按钮弹出提示
-document.querySelector('button').addEventListener('click', function() {
-    alert('恭喜！你的网页交互成功啦！');
-});
+// 点击按钮弹窗
+function showMessage() {
+  alert("交互成功！你点击了按钮");
+}
+
+// 表单提交
+function submitForm(e) {
+  e.preventDefault();
+  alert("表单提交成功！");
+}
+
+// 页面滚动效果（可自己加）
+window.onscroll = function() {
+  // 这里可以加返回顶部按钮
+};
